8e25203bef5b4d97b358b57d634be0c37b210d27
ChristineJackson464 <mom2mb464@gmail.com>
2022-04-29 09:11:51 -0400

    Initial submittal

---
+++ script.js
@@ -0,0 +1,40 @@
+/* Loading Component */
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+
+let load = 0
+
+let int = setInterval(blurring, 30)
+
+function blurring () {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+    }
+
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+
+}
+
+const scale = (num, in_min, in_max, out_min, out_max)  => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+/* Navbar 
+const nav = document.querySelector('.nav')
+
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
+*/
